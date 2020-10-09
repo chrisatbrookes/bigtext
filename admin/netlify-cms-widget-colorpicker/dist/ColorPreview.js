13e07dea79726b07aaa105a72006edd6873988ba
Chris Jennings <chrisjennings@Chriss-iMac.local>
2020-10-09 15:40:29 +0100

    added color picker

---
+++ admin/netlify-cms-widget-colorpicker/dist/ColorPreview.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = InlineSelectPreview;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function InlineSelectPreview(_ref) {
+  var value = _ref.value;
+  return /*#__PURE__*/_react["default"].createElement("div", null, value);
+}
+//# sourceMappingURL=ColorPreview.js.map
